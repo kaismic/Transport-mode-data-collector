6ff9f7e08ad11b32b92b93227cdcb4784c8ba3ce
Isac Kim <isac08270@gmail.com>
2026-06-18 18:17:34 +1000

    feat: add metro and tram transport modes

--- transport_data_collector/lib/core/transport_modes.dart
+++ transport_data_collector/lib/core/transport_modes.dart
@@ -16,9 +16,11 @@ const transportModes = <TransportMode>[
   TransportMode(id: 'car', label: 'Car', icon: Icons.directions_car),
   TransportMode(id: 'bus', label: 'Bus', icon: Icons.directions_bus),
   TransportMode(id: 'train', label: 'Train', icon: Icons.train),
+  TransportMode(id: 'metro', label: 'Metro', icon: Icons.directions_subway),
+  TransportMode(id: 'tram', label: 'Tram', icon: Icons.tram)
 ];
 
-const allowedVehicleTypes = {'car', 'bus', 'train'};
+const allowedVehicleTypes = {'car', 'bus', 'train', 'metro', 'tram'};
 
 TransportMode transportModeFor(String id) {
   return transportModes.firstWhere(
